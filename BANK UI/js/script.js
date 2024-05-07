39eccfba396373baeee86e3ae885c643d3ed523d
muhammad-ahmad-wpbrigade <151034968+muhammad-ahmad-wpbrigade@users.noreply.github.com>
2024-05-07 15:00:56 +0500

    Add files via upload

---
+++ BANK UI/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide the splash screen after some time (e.g., 9 seconds)
+    setTimeout(function() {
+        document.querySelector(".splash-screen").style.display = "none";
+    }, 9000); // Adjust the time as needed
+});
